e586a33e2504e5cad6ec61b8c4a8db412d5ddff6
Marya Faivisovich <z89262554967@gmail.com>
2023-02-19 20:36:10 +0300

    6

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-n = 6
+'''n = 6
 print(type(n))
 n = 'hbh" "bh'
 print(n)
 n = "bhb" , "hbh"
-print(n)
+print(n)'''
+
+'''print('введите первое число: ')
+a = input()
+print('введите второе число: ')
+b = input()
+print('введите третье число: ')
+c = input()
+print(a, '+', b, '+', c, '=', a+b+c)'''
