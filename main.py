ec5126b479efbe0c34aad66e567c6adc17b803eb
Marya373 <119407256+Marya373@users.noreply.github.com>
2023-02-20 08:43:24 +0300

    Dop_task_upload

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-'''n = 6
-print(type(n))
-n = 'hbh" "bh'
-print(n)
-n = "bhb" , "hbh"
-print(n)'''
-
-'''print('введите первое число: ')
-a = input()
-print('введите второе число: ')
-b = input()
-print('введите третье число: ')
-c = input()
-print(a, '+', b, '+', c, '=', a+b+c)'''
+c = 5.87
+print(c)
+print(type(c))
+c = int (c)
+print(c)
+print(type(c))
+c = str (c)
+print(c+'jndfjfnsj')
+print(type(c))
