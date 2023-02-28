cc6530bd242d5a9b2c1f2df23f2f1b175236d6af
Faivisovich Marya <z89262554967@gmail.com>
2023-02-28 18:12:41 +0300

    20_18

--- task18.py
+++ task18.py
@@ -7,3 +7,11 @@ N = int(input())
 lst = map(int, (input().split()))
 x = int(input())
 print(min(lst, key=lambda a:abs(a-x)))
+
+#a=[int(i) for i in input().split()]
+#b=int(input())
+#number=0
+#for i in range(len(a)):
+#   if (b-a[i])<b-number and b-a[i]>0:
+#       number=i
+#print(a[number])
