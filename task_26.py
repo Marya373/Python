3c7d0be7329d0fb8a56616c7db220fb6bb0621a4
Faivisovich Marya <z89262554967@gmail.com>
2023-03-04 15:54:50 +0300

    26

--- task_26.py
+++ task_26.py
@@ -5,7 +5,7 @@ def power(a, b):
     if (b == 1):
         return (a)
     if (b != 1):
-        return (a * power(a, b - 1))
+        return (a * power(a, b - 1)) #накапливаем произведение числа самого на себя
 a = int(input("Введите число: "))
 b = int(input("Введите его степень: "))
 print("Результат возведения в степень равен:", power(a, b))
