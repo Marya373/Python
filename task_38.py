db4e8f2d6ed25bf75c2210d23aeffc8a466c29a3
Faivisovich Marya <z89262554967@gmail.com>
2023-03-17 11:51:59 +0300

    38

--- task_38.py
+++ task_38.py
@@ -1,73 +1,88 @@
-#Дополнить телефонный справочник возможностью изменения и удаления данных. 
-#Пользователь также может ввести имя или фамилию, и Вы должны реализовать 
+# Создать телефонный справочник с возможностью импорта и экспорта данных в формате .txt. 
+# Фамилия, имя, отчество, номер телефона - данные, которые должны находиться в файле.
+
+# 1. Программа должна выводить данные
+# 2. Программа должна сохранять данные в текстовом файле
+# 3. Пользователь может ввести одну из характеристик для поиска о
+#    определенной записи(Например имя или фамилию человека)
+# 4. Использование функций. Ваша программа не должна быть линейной
+
+# ДЗ
+# Дополнить телефонный справочник возможностью изменения и удаления данных.
+# Пользователь также может ввести имя или фамилию, и Вы должны реализовать
 # функционал для изменения и удаления данных
 
 
-def show_data():
-    '''Эта функция показывает содержимое справочника'''
-    with open('data.txt', 'r', encoding='utf-8') as data:
-        return data.read().split('\n')
+def read_data(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        data = f.readlines()
+        data = list(map(lambda x: x.replace('\n', ''), data))
+        return data
+def input_data():
+    data = input('Введите Фамилию Имя Отчечство: ') + ' | '
+    data += input('Введите номер телефона: ')
+    return data
 
+def add_data(file, data):
+    with open(file, 'a', encoding='utf-8') as f:
+        f.write('\n' + data)
 
-def new_data():
-    '''Добавляет новую информацию в справочник'''
-    with open('data.txt', 'a', encoding='utf-8') as file:
-        file.write('\n' + input('Введите данные: '))
+def write_data(file, data):
+    with open(file, 'w', encoding='utf-8') as f:
+        f.write(data)
 
+def find_data(text) -> int:
+    data = read_data('data.txt')
+    result = [x for x in data if text in x]
+    if len(set(result)) == 1:
+        result = str(*result)
+        print(f'\nНашёл: {result} \n')
+        return data.index(result)
+    elif len(set(result)) >1:
+        print('\nТаких записей много, уточните поиск\n')
+        print_data(result)
+    else:
+        print("\nНе нашел :(\n")
 
-def find_data():
-    '''Эта функция ищет информацию в справочнике'''
-    with open('data.txt', 'r', encoding='utf-8') as file:
-        book = file.read().split('\n')
-        temp = input()
-        for i in book:
-            if temp in i:
-                print(i)
+def print_data(data):
+    print('\n'.join(data))
 
+def del_data(index):
+    if index:
+        data = read_data('data.txt')
+        print('Введите новые данные')
+        if input('Введите 1 для подтверждения удаления: ') == '1': 
+            data.pop(index)
+            to_write = '\n'.join(data)
+            write_data('data.txt', to_write)
+            print('Данные удалил')
+        else:
+            print('Не подтверждено')
 
-def replace_data():
-    '''Эта функция изменяет информацию в справочнике'''
-    with open('data.txt', 'r', encoding='utf-8') as file:
-        book = file.read().split('\n')
-        a = False
-        while a == False:
-            temp = input('Что меняем: ')
-            for i in range(len(book)):
-                if temp in book[i]:
-                    print(f'Что вы хотите сделать с записью ({book[i]})?\
-                          \n 1. Изменить\
-                          \n 2. Удалить\
-                          \n 3. Пропустить')
-                    user_ans = 3
-                    user_ans = int(input())
-                    if user_ans == 1:
-                        book[i] = book[i].replace(book[i], input('На что меняем: '))
-                    if user_ans == 2:
-                        book[i] = book[i].replace((book[i]), '')
-                    a = True
-            if a == False:
-                print('Запись не найдена')
-    with open('data.txt', 'w', encoding='utf-8') as file:
-        file.write('\n'.join(filter(lambda x: x != '', book)))
-
+def edit_data(index):
+    if index:
+        data = read_data('data.txt')
+        data[index] = input_data()
+        to_write = '\n'.join(data)
+        write_data('data.txt', to_write)
+        print('Данные заменил')
 
 while True:
-    mode = input('Выберите режим работы справочника:\
-                \n 1. Показать содержимое справочника\
-                \n 2. Добавить новую информацию в справочник\
-                \n 3. Поиск информации в справочнике\
-                \n 4. Изменить информацию в справочнике\
-                \n ')
-
-    if mode == '1':
-        print(show_data())
-    elif mode == '2':
-        new_data()
-    elif mode == '3':
-        find_data()
-    elif mode == '4':
-        replace_data()
-    elif mode == '0':
+    choice = input('\n   Прочитать данные - r \
+                   \n   Ввести данные - w \
+                   \n   Найти данные - f \
+                   \n   Изменить данные - e \
+                   \n   Удалить данные - d \
+                   \n   Выйти - 0 \
+                   \n   Выберите что надо сделать: ')
+    if choice == 'r': print_data(read_data('data.txt'))
+    elif choice == 'w': add_data('data.txt' ,input_data())
+    elif choice == 'f': index = find_data(input('Введите данные для поиска: '))
+    elif choice == 'd': del_data(find_data(input('Введите данные для поиска: ')))
+    elif choice == 'e': edit_data(find_data(input('Введите данные для поиска: ')))
+    elif choice == '0': 
+        print('Пока!')
         break
-    else:
-        print('No mode')
+    else: print('Ошибка, повторите выбор: ')
+
+    
